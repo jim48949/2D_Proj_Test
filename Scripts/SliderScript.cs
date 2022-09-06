318450d5b7e5bdfa213b2301a820b10f66baeb61
jim48949 <panjin48949@gmail.com>
2022-09-06 10:02:58 -0700

    Fixed frame rate issue

--- Scripts/SliderScript.cs
+++ Scripts/SliderScript.cs
@@ -40,7 +40,7 @@ public class SliderScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         // Slider Moving Part
         if (isForward && !stopMoving)
