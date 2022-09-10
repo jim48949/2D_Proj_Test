69ddce0540815a8ab1ac10a4bac2c25912c79702
jim48949 <panjin48949@gmail.com>
2022-09-09 22:10:10 -0700

    Rewrote slider script to improve hitzone calculation

--- Scripts/SliderScript.cs
+++ Scripts/SliderScript.cs
@@ -12,31 +12,60 @@ public class SliderScript : MonoBehaviour
     public Transform hitZoom; // Determine the position of the hit zoom
     public bool isFinished; // Global bool to tell whether the event has finished
 
+
     private bool isForward; // Used for judging which side the handle goes
     private float minHit; // Min value of hit zoom
     private float maxHit; // Max value of hit zoom
     private bool stopMoving; // Stop the handle movement
+    private RectTransform slider_Rect;
+    private RectTransform hitzone_Rect;
     
  
     // Start is called before the first frame update
     void Start()
     {
-        Reset();
+        slider_Rect = (RectTransform)slider.transform;
+        hitzone_Rect = (RectTransform)hitZoom;
+        Reset();     
     }
 
     // Reset all vars for next event
     public void Reset()
     {
+        float hitzoneHalf = hitzone_Rect.rect.width / 2.0f;
+        
         slider.value = 0;
         isForward = true;
         isFinished = false;
         stopMoving = false;
         hitImg.SetActive(false);
         missImg.SetActive(false);
-        float randomPosX = Random.Range(0, 480f); // 480 is the width of the slider
-        minHit = randomPosX >= 60 ? ((randomPosX - 60f) / 480) : (randomPosX / 480);
-        maxHit = randomPosX <= 292 ? ((randomPosX + 60f) / 480) : (randomPosX / 480);
-        hitZoom.localPosition = new Vector2(randomPosX - 240f, 0);
+        float randomPosX = Random.Range(0, slider_Rect.rect.width*1.0f); // width of the slider (e.g, width = 100, range = (0, 100))
+        // Calculate min and max for hitzone
+        // Note that randomRosX is the center pos of hitzone
+        // Both minHit and maxHit need to be divided by slider_Rect.rect.width to normalize their value in between [0, 1]
+        if (randomPosX >= (slider_Rect.rect.width - hitzoneHalf))
+        {
+            // Special Case 1: maxHit is outside the slide area
+            minHit = (slider_Rect.rect.width - hitzone_Rect.rect.width) / slider_Rect.rect.width;
+            maxHit = slider_Rect.rect.width / slider_Rect.rect.width;
+            hitZoom.localPosition = new Vector2(slider_Rect.rect.width / 2 - hitzoneHalf, 0);
+        }
+        else if (randomPosX <= hitzoneHalf)
+        {
+            // Special Case 2: minHit is outside the slide area
+            minHit = 0;
+            maxHit = hitzone_Rect.rect.width / slider_Rect.rect.width;
+            hitZoom.localPosition = new Vector2(-slider_Rect.rect.width / 2 + hitzoneHalf, 0);
+        }
+        else
+        {
+            // Normal Case
+            minHit = (randomPosX - hitzoneHalf) / slider_Rect.rect.width;
+            maxHit = (randomPosX + hitzoneHalf) / slider_Rect.rect.width;
+            hitZoom.localPosition = new Vector2(randomPosX - slider_Rect.rect.width / 2, 0);
+        }
+
     }
 
     // Update is called once per frame
@@ -74,14 +103,15 @@ public class SliderScript : MonoBehaviour
 
             if (slider.normalizedValue >= minHit && slider.normalizedValue <= maxHit)
             {
-                Debug.Log("Hit!");
+                //Debug.Log("Hit!");
                 hitImg.SetActive(true);
             }
             else
             {
-                Debug.Log("Missed!");
+                //Debug.Log("Missed!");
                 missImg.SetActive(true);
             }
+            Debug.Log("Hitzone Pos: " + hitZoom.localPosition);
             StartCoroutine(CountDown(3));
         }
 
